42124aa56c84ca972252d35e2e7d7d8ec65cfdbe
fmatuschke <f.matuschke@fz-juelich.de>
2020-10-03 12:55:42 +0200

    ADD menu template

--- gui/mainwindow.py
+++ gui/mainwindow.py
@@ -1,10 +1,9 @@
 import sys
 import os
-from PyQt5.QtCore import Qt, pyqtSignal, QSize
-from PyQt5.QtGui import QColor, QPixmap, QImage, QIcon
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QMessageBox
-from PyQt5.QtWidgets import QGridLayout, QLabel
-from PyQt5.QtWidgets import QMenuBar, QAction, qApp
+
+from PyQt5 import QtCore
+from PyQt5 import QtGui
+from PyQt5 import QtWidgets
 
 from gui.dummy import ImageWidget
 from gui.camerawidget import CameraWidget
@@ -13,7 +12,7 @@ pli_logo_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                              "pli-logo.png")
 
 
-class MainWindow(QMainWindow):
+class MainWindow(QtWidgets.QMainWindow):
     '''
     MainWindow of the application, which contains all widgets and sort them in a layout
     '''
@@ -23,9 +22,9 @@ class MainWindow(QMainWindow):
         self.__initUI__()
 
     def __initUI__(self):
-        self.setAttribute(Qt.WA_DeleteOnClose)
+        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
         self.setWindowTitle("3D-PLI Demo")
-        self.setWindowIcon(QIcon(pli_logo_path))
+        self.setWindowIcon(QtGui.QIcon(pli_logo_path))
         self.createMenue()
         self.createCentralWidget()
         self.connect_widgets()
@@ -36,34 +35,34 @@ class MainWindow(QMainWindow):
 
     def createCentralWidget(self):
         '''Creates Widgets and adds them to layout of centralWidget of MainWindow'''
-        self.layout = QGridLayout()
+        self.layout = QtWidgets.QGridLayout()
 
-        self.setCentralWidget(QWidget(self))
+        self.setCentralWidget(QtWidgets.QWidget(self))
 
         self.cam_resolution_target = (1280, 1024)
         self.camwidget = ImageWidget(self)
-        self.camwidget.setMinimumSize(QSize(600, 600))
+        self.camwidget.setMinimumSize(QtCore.QSize(600, 600))
 
         self.plotwidget = ImageWidget(self)
-        self.plotwidget.setMinimumSize(QSize(300, 300))
-        self.plotwidget.setMaximumSize(QSize(600, 600))
+        self.plotwidget.setMinimumSize(QtCore.QSize(300, 300))
+        self.plotwidget.setMaximumSize(QtCore.QSize(600, 600))
 
         self.zoomwidget = ImageWidget(self)
-        self.zoomwidget.setMinimumSize(QSize(300, 300))
-        self.zoomwidget.setMaximumSize(QSize(600, 600))
+        self.zoomwidget.setMinimumSize(QtCore.QSize(300, 300))
+        self.zoomwidget.setMaximumSize(QtCore.QSize(600, 600))
 
         self.tiltwidget = ImageWidget(self)
-        self.tiltwidget.setMinimumSize(QSize(100, 300))
-        self.tiltwidget.setMaximumSize(QSize(300, 500))
+        self.tiltwidget.setMinimumSize(QtCore.QSize(100, 300))
+        self.tiltwidget.setMaximumSize(QtCore.QSize(300, 500))
 
-        self.image = QImage(pli_logo_path)
-        self.logolabel = QLabel()
-        self.logolabel.setMaximumSize(QSize(100, 100))
-        self.logolabel.setMinimumSize(QSize(100, 100))
+        self.image = QtGui.QImage(pli_logo_path)
+        self.logolabel = QtWidgets.QLabel()
+        self.logolabel.setMaximumSize(QtCore.QSize(100, 100))
+        self.logolabel.setMinimumSize(QtCore.QSize(100, 100))
         self.logolabel.setPixmap(
-            QPixmap.fromImage(self.image).scaled(self.logolabel.size().width(),
-                                                 self.logolabel.size().height(),
-                                                 Qt.KeepAspectRatio))
+            QtGui.QPixmap.fromImage(self.image).scaled(
+                self.logolabel.size().width(),
+                self.logolabel.size().height(), QtCore.Qt.KeepAspectRatio))
 
         self.layout.addWidget(self.camwidget, 0, 0, 3, 1)
         self.layout.addWidget(self.zoomwidget, 0, 1, 1, 2)
@@ -73,9 +72,38 @@ class MainWindow(QMainWindow):
         self.centralWidget().setLayout(self.layout)
 
     def createMenue(self):
+
         self.statusBar()
         mainMenu = self.menuBar()
+
+        # PLI
+        pliMenu = mainMenu.addMenu('&PLI')
+        #
+        extractAction = QtWidgets.QAction("&live", self)
+        pliMenu.addAction(extractAction)
+        #
+        extractAction = QtWidgets.QAction("&transmittion", self)
+        pliMenu.addAction(extractAction)
+        #
+        extractAction = QtWidgets.QAction("&direction", self)
+        pliMenu.addAction(extractAction)
+        #
+        extractAction = QtWidgets.QAction("&retardation", self)
+        pliMenu.addAction(extractAction)
+        #
+        extractAction = QtWidgets.QAction("&fom", self)
+        pliMenu.addAction(extractAction)
+
+        # CAMERA
         cameraMenu = mainMenu.addMenu('&Camera')
+        #
+        extractAction = QtWidgets.QAction("&port", self)
+        # extractAction.setShortcut("Ctrl+C")
+        # extractAction.setStatusTip('Change camera device')
+        cameraMenu.addAction(extractAction)
+        #
+        extractAction = QtWidgets.QAction("&resolution", self)
+        cameraMenu.addAction(extractAction)
 
     def connect_widgets(self):
         '''Connect Signal and Slots of the Widgets for inter Widget communication'''
@@ -86,7 +114,7 @@ class MainWindow(QMainWindow):
         Sets the Background Color
         '''
         p = self.palette()
-        p.setColor(self.backgroundRole(), QColor(38, 65, 100))
+        p.setColor(self.backgroundRole(), QtGui.QColor(38, 65, 100))
         self.setPalette(p)
 
     def closeWidgets(self):
@@ -96,11 +124,12 @@ class MainWindow(QMainWindow):
         '''
         Show a user dialog when window is closed
         '''
-        reply = QMessageBox.question(
+        reply = QtWidgets.QMessageBox.question(
             self, 'Window Close', 'Are you sure you want to close the window?',
-            QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+            QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No,
+            QtWidgets.QMessageBox.No)
 
-        if reply == QMessageBox.Yes:
+        if reply == QtWidgets.QMessageBox.Yes:
             self.closeWidgets()
             event.accept()
             print('Window closed')
