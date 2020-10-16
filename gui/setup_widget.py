325a4e9efb1f8b4c9e8c9453b0af6d3d2d5207db
fmatuschke <f.matuschke@fz-juelich.de>
2020-10-16 15:58:02 +0200

    RFC opengl animation

--- gui/setup_widget.py
+++ gui/setup_widget.py
@@ -17,7 +17,15 @@ class SetupWidget(QtOpenGL.QGLWidget):
         super(SetupWidget, self).__init__(parent)
         self._tilt_angle = (0, 10)
         self._rotation = 0
-        self._tex = [self.read_texture("src/test.jpg")]
+        self._tex = {
+            "lines": self.get_pattern_lines(),
+            "green": self.get_pattern_green(),
+            "cross": self.get_pattern_cross(),
+            "polfilter": self.get_pattern_polfilter(),
+            "section": self.read_texture("src/section.png"),
+            "section_green": self.read_texture("src/section_green.png"),
+            "test": self.get_pattern_test()
+        }
 
         # p = self.palette()
         # p.setColor(self.backgroundRole(), QtGui.QColor(255, 0, 0))
@@ -54,11 +62,19 @@ class SetupWidget(QtOpenGL.QGLWidget):
         glViewport(0, 0, w, h)
         glMatrixMode(GL_MODELVIEW)
 
+        self._quadObj = gluNewQuadric()
+        gluQuadricTexture(self._quadObj, GL_TRUE)
+        glEnable(GL_TEXTURE_2D)
+        self._tex_dict = self.bind_texture(self._quadObj)
+
     def paintGL(self):
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         glLoadIdentity()
         glTranslatef(0, 0, -50)  # pos camera
+        glRotatef(10, 1, 0, 0)
+        glRotatef(-20, 0, 1, 0)
         self.drawOpticalElements()
+        self.drawOtherElements()
         glFlush()
 
     def set_rotation(self, rot):
@@ -68,40 +84,81 @@ class SetupWidget(QtOpenGL.QGLWidget):
         self._tilt_angle = (float(theta), float(phi))
 
     def read_texture(self, file_name):
-        print("READ TEST")
         if os.path.isfile(file_name):
             img = Image.open(file_name)
-            return np.array(img, np.uint8)
+            img = np.array(img, np.uint8)
+            return img
         print(f"WARNING: texture '{file_name}' not found")
         return None
 
-    def bind_texture(self, quadObj, tex):
-        gluQuadricTexture(quadObj, GL_TRUE)
-        glEnable(GL_TEXTURE_2D)
-        textID = glGenTextures(1)
-        glBindTexture(GL_TEXTURE_2D, textID)
-        glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP)
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP)
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-        glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL)
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, tex.shape[1], tex.shape[0], 0,
-                     GL_RGB, GL_UNSIGNED_BYTE, tex.ravel())
-        glBindTexture(GL_TEXTURE_2D, textID)
+    def get_pattern_test(self):
+        texture = np.zeros((16, 16, 3), np.uint8)
+        v = np.mgrid[0:256].reshape(16, 16)
+        texture[:, :, 0] = v
+        texture[:, :, 1] = v.T
+        return texture
+
+    def get_pattern_lines(self):
+        texture = np.zeros((180, 180, 4), np.uint8)
+        texture[None, None, :] = [180, 180, 180, 25]
+        for i in range(0, texture.shape[1], 10):
+            texture[:, i, 0] = 0
+            texture[:, i, 1] = 0
+            texture[:, i, 2] = 0
+            texture[:, i, 3] = 125
+        return texture
+
+    def get_pattern_green(self):
+        texture = np.zeros((180, 180, 4), np.uint8)
+        texture[None, None, :] = [180, 180, 180, 25]
+        return texture
+
+    def get_pattern_cross(self):
+        texture = self.get_pattern_green()
+        texture[:, texture.shape[1] // 2 - 5:texture.shape[1] // 2 +
+                5, :] = [0, 0, 0, 125]
+        texture[texture.shape[0] // 2 - 5:texture.shape[0] // 2 +
+                5, :, :] = [0, 0, 0, 125]
+        return texture
+
+    def get_pattern_polfilter(self):
+        texture = self.get_pattern_green()
+        texture[None, texture.shape[1] // 2 - 5:texture.shape[1] // 2 +
+                5, :] = [0, 0, 0, 125]
+        return texture
+
+    def bind_texture(self, quadObj):
+
+        textID = glGenTextures(len(self._tex))
+        tex_dict = {}
+        for tex_id, (tex_name, tex) in zip(textID, self._tex.items()):
+            glBindTexture(GL_TEXTURE_2D, tex_id)
+            glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
+            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP)
+            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP)
+            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+            glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL)
+            if tex.shape[2] == 3:
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, tex.shape[1],
+                             tex.shape[0], 0, GL_RGB, GL_UNSIGNED_BYTE,
+                             tex.ravel())
+            if tex.shape[2] == 4:
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, tex.shape[1],
+                             tex.shape[0], 0, GL_RGBA, GL_UNSIGNED_BYTE,
+                             tex.ravel())
+            glBindTexture(GL_TEXTURE_2D, tex_id)
+            tex_dict[tex_name] = tex_id
+        return tex_dict
 
     def drawOpticalElements(self):
-        quadObj = gluNewQuadric()
-        if self._tex[0] is not None:
-            self.bind_texture(quadObj, self._tex[0])
-
         h = 0.5
         dy = 3
         r = 5
         NUM_POLYGON = 42
-        glColor3f(0.2, 0.6, 0.2)
+        glColor3f(0.7, 0.7, 0.7)
         for i in range(4):
             glPushMatrix()
             y = -1.5 * dy - h // 2 + i * dy
@@ -113,9 +170,75 @@ class SetupWidget(QtOpenGL.QGLWidget):
                 glRotatef(self._tilt_angle[0], 0.0, 0.0, 1.0)  # tilting
                 glRotatef(self._tilt_angle[1], 0.0, 1.0, 0.0)  # tilting
                 glRotatef(-self._tilt_angle[0], 0.0, 0.0, 1.0)  # tilting
-            gluDisk(quadObj, 0, r, NUM_POLYGON, 1)
-            gluCylinder(quadObj, r, r, h, NUM_POLYGON, 1)
+            # bottom
+            if i == 0:
+                glBindTexture(GL_TEXTURE_2D, self._tex_dict["polfilter"])
+            if i == 1:
+                glBindTexture(GL_TEXTURE_2D, self._tex_dict["cross"])
+            if i == 2:
+                glBindTexture(GL_TEXTURE_2D, self._tex_dict["section_green"])
+            if i == 3:
+                glRotatef(90, 0.0, 0.0, 1.0)  # filter rho
+                glBindTexture(GL_TEXTURE_2D, self._tex_dict["polfilter"])
+            gluDisk(self._quadObj, 0, r, NUM_POLYGON, 1)
+            # cylinder
+            glBindTexture(GL_TEXTURE_2D, self._tex_dict["lines"])
+            gluCylinder(self._quadObj, r, r, h, NUM_POLYGON, 1)
+            # top
             glTranslatef(0, 0, h)
-            gluDisk(quadObj, 0, r, NUM_POLYGON, 1)
+            if i == 0:
+                glBindTexture(GL_TEXTURE_2D, self._tex_dict["polfilter"])
+            if i == 1:
+                glBindTexture(GL_TEXTURE_2D, self._tex_dict["cross"])
+            if i == 2:
+                glRotatef(180, 0.0, 0.0, 1.0)  # filter rho
+                glBindTexture(GL_TEXTURE_2D, self._tex_dict["section_green"])
+            if i == 3:
+                glRotatef(90, 0.0, 0.0, 1.0)  # filter rho
+                glBindTexture(GL_TEXTURE_2D, self._tex_dict["polfilter"])
+            gluDisk(self._quadObj, 0, r, NUM_POLYGON, 1)
 
             glPopMatrix()
+
+    def drawOtherElements(self):
+        glColor3f(0.2, 0.8, 0.2)
+        glBegin(GL_POLYGON)
+        # glNormal3f(0.0, 1.0, 0.0)
+        glVertex3f(-5, -8, 5)
+        glVertex3f(-5, -8, -5)
+        glVertex3f(5, -8, -5)
+        glVertex3f(5, -8, 5)
+        glEnd()
+
+        glTranslatef(0, 10, 0)
+        glColor3f(0.3, 0.3, 0.3)
+        h = 2
+        wy = 2
+        wx = 2
+        glBegin(GL_QUADS)
+        for z in [-1.0, 1.0]:
+            # glNormal3f(0.0, -z, 0.0)
+            glVertex3f(wx, z * h, -wy)
+            glVertex3f(wx, z * h, wy)
+            glVertex3f(-wx, z * h, wy)
+            glVertex3f(-wx, z * h, -wy)
+
+        for x in [-1.0, 1.0]:
+            # glNormal3f(x, 0.0, 0.0)
+            glVertex3f(x * wx, h, wy)
+            glVertex3f(x * wx, h, -wy)
+            glVertex3f(x * wx, -h, -wy)
+            glVertex3f(x * wx, -h, wy)
+
+        for y in [-1.0, 1.0]:
+            # glNormal3f(0.0, 0.0, -y)
+            glVertex3f(-wx, h, y * wy)
+            glVertex3f(-wx, -h, y * wy)
+            glVertex3f(wx, -h, y * wy)
+            glVertex3f(wx, h, y * wy)
+        glEnd()
+
+        gobj = gluNewQuadric()
+        glTranslatef(0, -h, 0)
+        glRotatef(90, 1, 0, 0)
+        gluCylinder(self._quadObj, 0.9, 1, 0.5, 42, 1)
