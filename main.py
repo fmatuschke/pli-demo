aba7c334559770c773624feca4985e05af03a9cd
fmatuschke <f.matuschke@fz-juelich.de>
2021-04-29 18:56:07 +0200

    ADD executable main

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QWidget
 
 app = QApplication(sys.argv)
+window = QWidget()
+window.show()
 sys.exit(app.exec_())
