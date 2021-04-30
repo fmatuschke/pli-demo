a0bc97e1c65e82ff5af86e2d337422eb7b0c3376
Gitlab Runner <runner@fz-juelich.de>
2021-04-30 14:17:17 +0200

    ADD video file posibility

--- src/capture_device.py
+++ src/capture_device.py
@@ -3,6 +3,7 @@ import os
 
 import cv2
 import numpy as np
+import time
 
 # cv2 workaround
 # https://forum.qt.io/topic/119109/using-pyqt5-with-opencv-python-cv2-causes-error-could-not-load-qt-platform-plugin-xcb-even-though-it-was-found/23
@@ -31,17 +32,17 @@ class CapDev:
     def __freeze(self):
         self.__is_frozen = True
 
-    def __init__(self,
-                 width=1280,
-                 height=960,
-                 fps=25,
-                 port=0,
-                 port_list=range(5),
-                 color_mode=Color.GREEN):
+    def __init__(
+            self,
+            port=0,
+            port_list=range(5),
+            file_name=None,  # 'data/half_720p.mp4'
+            color_mode=Color.GREEN):
 
         self._device = None
         self._port = port
         self._ports = []
+        self._resolutions = []
         self._color_mode = color_mode
 
         # freeze class
@@ -49,12 +50,33 @@ class CapDev:
 
         # run init member functions
         self._ports = self._check_ports(port_list, n_times=5)
-        if self._port not in self._ports:
+
+        if file_name is not None:
+            self.activate_video(file_name)
+        elif self._port in self._ports:
+            self.activate_camera(self._port)
+        else:
             self._device = None
             self._port = None
+            self.release_device()
+
+    def release_device(self):
+        if self._device is not None:
+            if self._device.isOpened():
+                self._device.release()
+
+    def activate_camera(self, port):
+        self.release_device()
+        if self._check_port(port):
+            self._device = cv2.VideoCapture(port)
+            # self._check_resolutions() # takes time
         else:
-            print("setting port")
-            self._device = cv2.VideoCapture(self._port)
+            print("Error: port is not working")
+
+    def activate_video(self, file_name):
+        self.release_device()
+        self._device = cv2.VideoCapture(file_name)
+        # TODO: catch input error
 
     def _check_port(self, port, n_times=1):
         camera = cv2.VideoCapture(port)
@@ -68,6 +90,7 @@ class CapDev:
                 camera.release()
                 break
 
+        self.release_device()
         return port_works
 
     def _check_ports(self, port_list, n_times=1):
@@ -76,18 +99,85 @@ class CapDev:
         return ports
 
     def set_port(self, port):
-        self._device = cv2.VideoCapture(i)
+        self.release_device()
+        self._device = cv2.VideoCapture(port)
         if not self._device.isOpened():
             print(f'camera port "{port}" not opening')
         else:
             self._port = port
 
+    def _check_resolutions(self):
+        # TODO this should be available from the OS
+
+        org_width = int(self._device.get(cv2.CAP_PROP_FRAME_WIDTH))
+        org_height = int(self._device.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        org_fps = int(self._device.get(cv2.CAP_PROP_FPS))
+        self._resolutions = [(org_width, org_height, org_fps)]
+
+        # test_resolution = [(320, 480), (640, 480), (800, 600), (1024, 786),
+        #                    (1280, 720), (1280, 800), (1280, 960),
+        #                    (1280, 1024), (1440, 720), (1440, 900),
+        #                    (1600, 1200), (1920, 1080), (1920, 1200),
+        #                    (2560, 1440), (2560, 1600), (3840, 2160)]
+
+        # EXIO cam USB2
+        test_resolution = [(640, 480), (1024, 768), (1280, 720), (1280, 960),
+                           (1920, 1080), (2048, 1536), (2560, 1440),
+                           (2560, 1600), (3840, 2160)]
+
+        # reset
+        # self.set_port(self._port)
+
+        print(f'INFO: start testing vor properties')
+        for width, height in test_resolution:
+            self.set_prop(width, height, 60)
+            width = int(self._device.get(cv2.CAP_PROP_FRAME_WIDTH))
+            height = int(self._device.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            fps = int(self._device.get(cv2.CAP_PROP_FPS))
+            self._resolutions.append((width, height, fps))
+
+        self._resolutions = list(set(self._resolutions))
+        self._resolutions.sort(key=lambda x: x[1])
+        self._resolutions.sort(key=lambda x: x[0])
+        print(f'INFO: found {self._resolutions}')
+
+        # reset to original
+        self.set_prop(org_width, org_height, org_fps)
+
+        return self._resolutions
+
+    def set_prop(self, width, height, fps=25):
+        # reset camera to set setting before image capture
+        # self.set_port(self._port)
+
+        self._device.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        self._device.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+        self._device.set(cv2.CAP_PROP_FPS, fps)
+
+        # Read some images to ensure the cam is ready
+        for _ in range(42):
+            if self._device.read()[0]:
+                break
+            print('INFO: trying to get image after setting options')
+            time.sleep(0.42)
+
+    def empty_frame(self):
+        frame = np.zeros((128, 128), dtype=np.uint8)
+        np.fill_diagonal(frame, 255)
+        frame = np.flip(frame, 0)
+        np.fill_diagonal(frame, 255)
+        return frame
+
     def get_frame(self, quadratic=False):
 
         if self._device is None:
-            return None
+            return self.empty_frame()
 
         success, frame = self._device.read()
+
+        if not success:  # can be end of video, try to reset frame to pos=0
+            self._device.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            success, frame = self._device.read()
 
         if not success:
             print('Error: camera disconnected')
@@ -95,7 +185,7 @@ class CapDev:
             # In that case, this code shows error. You can check whether
             # it is initialized or not by the method cap.isOpened(). If
             # it is True, OK. Otherwise open it using cap.open().
-            return None
+            return self.empty_frame()
 
         if self._color_mode in [Color.GRAY, Color.RGB]:
             frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
