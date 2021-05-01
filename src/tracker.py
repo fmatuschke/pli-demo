4504050a245fed1a173a2099d58c8cbbc41ec719
fmatuschke <f.matuschke@fz-juelich.de>
2021-05-01 10:56:55 +0200

    ADD class skeleton

--- src/tracker.py
+++ src/tracker.py
@@ -1,2 +1,20 @@
+import typing
+
+
 class Tracker:
-    pass
+    __is_frozen = False
+
+    def __setattr__(self, key, value):
+        if self.__is_frozen and not hasattr(self, key):
+            raise TypeError('%r is a frozen class' % self)
+        object.__setattr__(self, key, value)
+
+    def __freeze(self):
+        self.__is_frozen = True
+
+    def __init__(self):
+        self.reset()
+        self.__freeze()
+
+    def reset(self):
+        pass
