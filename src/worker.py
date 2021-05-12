26a0d0927c89eef42e8b84f31c687e597ccf0abe
Gitlab Runner <runner@fz-juelich.de>
2021-05-12 11:46:56 +0200

    FIX overflowing angle in plot

--- src/worker.py
+++ src/worker.py
@@ -245,7 +245,12 @@ class MainThread():
                     self.pli.images(self._tilt.value)[y, x,
                                                       self.pli.valid()])
             self.parent.plotwidget.update_data(x_data, y_data)
-        self.parent.plotwidget.update_rho(self._angle + self.pli.rotations[0])
+
+        val = self._angle + self.pli.rotations[0]
+        val %= np.pi
+        val += np.pi
+        val %= np.pi
+        self.parent.plotwidget.update_rho(val)
 
     def update_gui(self):
         self.parent.setupwidget.set_rotation(self._angle)
